6fb29000c5b4b9a3907e9735f2f16a732b7bea31
Ben Hertzler <bwhertzler@outlook.com>
2024-09-17 19:26:19 -0400

    edited comments

--- CSC 222 Programming Basics Project.cpp
+++ CSC 222 Programming Basics Project.cpp
@@ -1,5 +1,9 @@
 // CSC 222 Programming Basics Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+
+// Ben Hertzler
+// Programming Basics Project
+// Master
 
 #include <iostream>
 
