a2339003896a4fc2e7a708910cb3b04087a9b688
Ben Hertzler <bwhertzler@outlook.com>
2024-09-17 20:25:19 -0400

    initial commit

--- CSC 222 Programming Basics Project.cpp
+++ CSC 222 Programming Basics Project.cpp
@@ -3,11 +3,30 @@
 
 // Ben Hertzler
 // Programming Basics Project
-// Master
+// Project 1: Distance Per Tank of Gas
+
+// Required User Input: None.
+// Program Output: Calculates the total distance a car can travel on one tank of gas.
+// Supplied Constants: Size of gas tank (20 gal.), urban fuel efficiency (23.5 mpg), highway fuel efficiency (28.9 mpg)
 
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Variables
+    double town_range, highway_range;
+
+    // Constants
+    const double TANK_GAL = 20, TOWN_MPG = 23.5, HIGHWAY_MPG = 28.9;
+
+    town_range = TANK_GAL * TOWN_MPG;
+    highway_range = TANK_GAL * HIGHWAY_MPG;
+
+    cout << "Maximum driving distance for a " << TANK_GAL << " gallon tank of gas:" << endl;
+    cout << "In Town: " << town_range << " miles." << endl;
+    cout << "On the Highway: " << highway_range << " miles." << endl;
+
+    return 0;
 }
